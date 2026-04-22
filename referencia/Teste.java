8372840d13641907f3d0a213fec21de1ac941b50
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-04-22 10:21:24 -0300

    dev: testando double & long

--- referencia/Teste.java
+++ referencia/Teste.java
@@ -1,13 +1,16 @@
 public class Teste {
     private final static int ANSWER = 42; 
     private final static float PI = 3.14f;
+    private final static long LONG = -42L;
 
     private static void hello() {
       System.out.println("Hello world!");
     }
 
     public static void main(String[] args) {
+        hello();
         System.out.println(ANSWER);
         System.out.println(PI);
+        System.out.println(LONG);
     }
 }
