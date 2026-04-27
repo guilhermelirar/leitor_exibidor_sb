d64a79840814be5f56cb95ead02dafba39e62e57
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-04-27 11:34:51 -0300

    disassembler: obtendo nomes relacionados aos opcodes

--- include/bytecode.h
+++ include/bytecode.h
@@ -3,11 +3,9 @@
 
 #include "classfile.h"
 
-void parse_code_attribute(
-  const attribute_info *attr,
-  Code_attribute* code_attr
-);
-
-void free_code_attribute(Code_attribute *code);
+typedef struct {
+  const char* name;
+  u1 operands;
+} bytecode_info;
 
 #endif
--- src/bytecode.c
+++ src/bytecode.c
@@ -1,1 +1,5 @@
+#include "bytecode.h"
 
+const bytecode_info bytecode_table[256] = {
+#include "opcodes.def"
+};
